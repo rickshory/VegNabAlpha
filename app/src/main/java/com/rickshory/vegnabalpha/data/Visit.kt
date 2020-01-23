d8895f1b2ea6a1dfb09304c23fe1f4ba17155c63
Rick Shory <rickshory@gmail.com>
2020-01-23 12:00:42 -0800

    flesh out Visit

--- app/src/main/java/com/rickshory/vegnabalpha/data/Visit.kt
+++ app/src/main/java/com/rickshory/vegnabalpha/data/Visit.kt
@@ -1,4 +1,13 @@
 package com.rickshory.vegnabalpha.data
+/**
+ * Immutable model class for a Visit. In order to compile with Room, we can't use @JvmOverloads to
+ * generate multiple constructors.
+ *
+ * @param name       name of the visit
+ * @param notes  notes on the visit
+ * @param isDone whether or not this visit is finished
+ * @param id          id of the visit
+ */
+data class Visit @JvmOverloads constructor {
 
-class Visit {
 }
