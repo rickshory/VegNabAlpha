b7e02f42efa603672744cf851c8e288f617090c3
Rick Shory <rickshory@gmail.com>
2020-01-23 12:06:30 -0800

    start to define Visit fields

--- app/src/main/java/com/rickshory/vegnabalpha/data/Visit.kt
+++ app/src/main/java/com/rickshory/vegnabalpha/data/Visit.kt
@@ -1,4 +1,9 @@
 package com.rickshory.vegnabalpha.data
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
 /**
  * Immutable model class for a Visit. In order to compile with Room, we can't use @JvmOverloads to
  * generate multiple constructors.
@@ -8,6 +13,12 @@ package com.rickshory.vegnabalpha.data
  * @param isDone whether or not this visit is finished
  * @param id          id of the visit
  */
-data class Visit @JvmOverloads constructor {
+@Entity(tableName = "visits")
+data class Visit @JvmOverloads constructor (
+    @ColumnInfo(name = "name") var name: String = "",
+    @ColumnInfo(name = "notes") var notes: String = "",
+    @ColumnInfo(name = "isDone") var isDone: Boolean = false,
+    @PrimaryKey @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString()
+) {
 
 }
