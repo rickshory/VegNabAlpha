69c9a3231c1fd665832854da69f606e77bd71123
Rick Shory <rickshory@gmail.com>
2020-01-24 19:57:20 -0800

    start set up database

--- app/src/main/java/com/rickshory/vegnabalpha/data/source/local/VegNabDatabase.kt
+++ app/src/main/java/com/rickshory/vegnabalpha/data/source/local/VegNabDatabase.kt
@@ -1,4 +1,14 @@
 package com.rickshory.vegnabalpha.data.source.local
 
-class VegNabDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.rickshory.vegnabalpha.data.Visit
+
+/**
+ * The Room Database that contains the VegNab tables
+ * Make exportSchema = true in production database
+ */
+@Database(entities = [Visit::class], version = 1, exportSchema = false)
+abstract class VegNabDatabase : RoomDatabase() {
+    abstract fun visitDao(): VisitsDao
 }
