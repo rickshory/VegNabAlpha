5af904a5292320a572cc37dc34a490420850e42e
Rick Shory <rickshory@gmail.com>
2020-01-24 19:50:40 -0800

    first Visits query

--- app/src/main/java/com/rickshory/vegnabalpha/data/source/local/VisitsDao.kt
+++ app/src/main/java/com/rickshory/vegnabalpha/data/source/local/VisitsDao.kt
@@ -1,10 +1,20 @@
 package com.rickshory.vegnabalpha.data.source.local
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Query
+import com.rickshory.vegnabalpha.data.Visit
+
 /**
  * Data Access Object for the visits table.
  */
 @Dao
 interface VisitsDao {
+    /**
+     * Observes list of visits.
+     * @return all visits.
+     */
+    @Query("SELECT * FROM Visits")
+    fun observeVisits(): LiveData<List<Visit>>
 
 }
