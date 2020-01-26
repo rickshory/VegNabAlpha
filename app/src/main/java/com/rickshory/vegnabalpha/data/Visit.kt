c3125c7d26ae5a0b38dcb26e5a2dead215a7abdf
Rick Shory <rickshory@gmail.com>
2020-01-26 07:21:59 -0800

    primary key autoincrement long

--- app/src/main/java/com/rickshory/vegnabalpha/data/Visit.kt
+++ app/src/main/java/com/rickshory/vegnabalpha/data/Visit.kt
@@ -8,17 +8,17 @@ import java.util.UUID
  * Immutable model class for a Visit. In order to compile with Room, we can't use @JvmOverloads to
  * generate multiple constructors.
  *
+ * @param id          id of the visit
  * @param name       name of the visit
  * @param notes  notes on the visit
  * @param isDone whether or not this visit is finished
- * @param id          id of the visit
  */
 @Entity(tableName = "visits")
 data class Visit @JvmOverloads constructor (
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "visitid") var id: Long = 0,
     @ColumnInfo(name = "name") var name: String = "",
     @ColumnInfo(name = "notes") var notes: String = "",
-    @ColumnInfo(name = "isDone") var isDone: Boolean = false,
-    @PrimaryKey @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString()
+    @ColumnInfo(name = "isDone") var isDone: Boolean = false
 ) {
 
 }
