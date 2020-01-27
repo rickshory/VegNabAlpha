37f22df5b710512857a743b56aec7e7b56a740ee
Rick Shory <rickshory@gmail.com>
2020-01-27 05:38:08 -0800

    Spp tables in DB

--- app/src/main/java/com/rickshory/vegnabalpha/data/source/local/VegNabDatabase.kt
+++ app/src/main/java/com/rickshory/vegnabalpha/data/source/local/VegNabDatabase.kt
@@ -2,13 +2,16 @@ package com.rickshory.vegnabalpha.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.rickshory.vegnabalpha.data.NrcsSpp
+import com.rickshory.vegnabalpha.data.NrcsSppFts
 import com.rickshory.vegnabalpha.data.Visit
 
 /**
  * The Room Database that contains the VegNab tables
  * Make exportSchema = true in production database
  */
-@Database(entities = [Visit::class], version = 1, exportSchema = false)
+@Database(entities = [NrcsSpp::class, NrcsSppFts::class, Visit::class], version = 1, exportSchema = false)
 abstract class VegNabDatabase : RoomDatabase() {
     abstract fun visitDao(): VisitsDao
+    abstract fun nrcsSppDao(): NrcsSppDao
 }
