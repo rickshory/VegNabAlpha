70c3f2f8bdd53f242daf3fdbde5e8b93360971f9
Rick Shory <rickshory@gmail.com>
2020-01-27 05:21:09 -0800

    separate fts from base content

--- app/src/main/java/com/rickshory/vegnabalpha/data/NrcsSppFts.kt
+++ app/src/main/java/com/rickshory/vegnabalpha/data/NrcsSppFts.kt
@@ -3,27 +3,26 @@ package com.rickshory.vegnabalpha.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Fts4
-import androidx.room.PrimaryKey
 
 /**
  * Immutable model class for an NRCS Species record. In order to compile with Room, we can't use @JvmOverloads to
  * generate multiple constructors.
+ * Full Text Search virtual table, based on NrcsSpp
  *
  * @param id          id of the record
  * @param code       standard code for the plant
  * @param genus       plant genus
  * @param species  species of the plant
  * @param vernacular common names(s) of the plant
- *
- *  This is the base table. The virtual table for full text search is 'nrcsspp_fts'
  */
 
-@Entity(tableName = "nrcsspp")
-data class NrcsSpp @JvmOverloads constructor  (
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "sppid") var id: Int = 0,
+@Fts4(contentEntity = NrcsSpp::class)
+@Entity(tableName = "nrcsspp_fts")
+data class NrcsSppFts @JvmOverloads constructor  (
+//    @PrimaryKey @ColumnInfo(name = "rowid") val id: Int, // understood, don't need explicit for FTS tables
     @ColumnInfo(name = "code") var code: String = "",
     @ColumnInfo(name = "genus") var genus: String = "",
     @ColumnInfo(name = "species") var species: String = "",
     @ColumnInfo(name = "vernacular") var vernacular: String = ""
-)  {
+)   {
 }
