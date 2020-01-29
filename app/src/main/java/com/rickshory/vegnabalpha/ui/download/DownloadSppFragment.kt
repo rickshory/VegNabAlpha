0c2607ac8a731aade15ba5bb043c5d9077c61c64
Rick Shory <rickshory@gmail.com>
2020-01-29 06:37:45 -0800

    paste in create methods

--- app/src/main/java/com/rickshory/vegnabalpha/ui/download/DownloadSppFragment.kt
+++ app/src/main/java/com/rickshory/vegnabalpha/ui/download/DownloadSppFragment.kt
@@ -1,4 +1,30 @@
 package com.rickshory.vegnabalpha.ui.download
 
-class DownloadSppFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.rickshory.vegnabalpha.R
+
+class DownloadSppFragment : Fragment()  {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+
+        // TODO Remove the line below when observeAuthenticationState is implemented
+        binding.authButton.text = getString(R.string.login_button_text)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeAuthenticationState()
+        binding.authButton.setOnClickListener { launchSignInFlow() }
+    }
 }
